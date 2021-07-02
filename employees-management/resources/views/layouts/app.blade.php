0b71f725961b581c88704360cb4557aa79ecda3d
Jim97M <wabuyajames@gmail.com>
2021-07-02 16:24:57 +0300

    Login Controller Module

--- employees-management/resources/views/layouts/app.blade.php
+++ employees-management/resources/views/layouts/app.blade.php
@@ -69,6 +69,7 @@
                                 <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
                                     @csrf
                                 </form>
+                               
                             </div>
                         </li>
                         @endguest
